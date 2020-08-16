0f2946d6e3b0f18b67281c31b774e642a8ef1267
James Miller <james.rex.miller.4@gmail.com>
2020-08-16 09:59:04 -0600

    Create files for refactoring - sixPack requirement

---
+++ 99bottles_js/lib/sixPack.js
@@ -0,0 +1,44 @@
+import { downTo } from './helpers';
+
+export class Bottles {
+  verse(numBottles) {
+    if (numBottles === 0) {
+      return 'No more bottles of beer on the wall, ' +
+      'no more bottles of beer.\n' +
+      'Go to the store and buy some more, ' +
+      '99 bottles of beer on the wall.\n';
+      
+    }
+    
+    let bottleVar = numBottles - 1 > 1 ? 'bottles' : 'bottle';
+
+    if (numBottles >= 2) {
+      return `${numBottles} bottles of beer on the wall, ` +
+      `${numBottles}` + ' bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      `${numBottles - 1}` + ` ${bottleVar} ` + 'of beer on the wall.\n';
+    } else {
+      return '1 bottle of beer on the wall, ' +
+      '1 bottle of beer.\n' +
+      'Take it down and pass it around, ' +
+      'no more bottles of beer on the wall.\n';
+    }
+  }
+  
+  verses(val1, val2) {
+    let response;
+    for (let i = val1; i >= val2; i--) {
+      let verse = this.verse(i);
+      if (i === val1) {
+        response = verse;
+      } else {
+        response = response + '\n' + verse;
+      }
+    }
+    return response;
+  }
+
+  song() {
+    return this.verses(99, 0);
+  }
+}
