af923d679c4e600376a55d8d4cd609facb18bd41
James Miller <james.rex.miller.4@gmail.com>
2020-09-21 07:30:33 -0600

    Add starter logic to files for other99 requirement

---
+++ 99bottles_js/lib/other99.js
@@ -0,0 +1,106 @@
+import { downTo, capitalize } from './helpers';
+
+export class Bottles {
+  verse(numBottles) {
+    const bottleNumber = BottleNumber.for(numBottles);
+
+    return (
+      capitalize(`${bottleNumber} of beer on the wall, `) +
+      `${bottleNumber} of beer.\n` +
+      `${bottleNumber.action()}`   +
+      `${bottleNumber.successor()} of beer on the wall.\n`
+    );
+  }
+  
+  verses(starting, ending) {
+    return downTo(starting, ending)
+      .map(i => this.verse(i))
+      .join('\n');
+  }
+
+  song() {
+    return this.verses(99, 0);
+  }
+}
+
+class BottleNumber {
+  constructor(number) {
+    this.number = number;
+  }
+
+  static for(number) {
+    const bottleNumberClass = BottleNumber.registry.find(
+      candidate => candidate.canHandle(number)
+    );
+    return new bottleNumberClass(number);
+  }
+
+  static register(candidate) {
+    BottleNumber.registry.unshift(candidate);
+  }
+
+  container() {
+    return 'bottles';
+  }
+
+  pronoun() {
+    return 'one';
+  }
+
+  quantity() {
+    return this.number.toString();
+  }
+
+  action() {
+    return `Take ${this.pronoun()} down and pass it around, `;
+  }
+
+  successor() {
+    return BottleNumber.for(this.number - 1);
+  }
+
+  toString() {
+    return `${this.quantity()} ${this.container()}`;
+  }
+}
+
+BottleNumber.registry = [BottleNumber];
+
+class BottleNumber0 extends BottleNumber {
+  quantity() {
+    return 'no more';
+  }
+
+  action() {
+    return 'Go to the store and buy some more, ';
+  }
+
+  successor() {
+    return BottleNumber.for(99);
+  }
+}
+
+BottleNumber.register(BottleNumber0);
+class BottleNumber1 extends BottleNumber {
+  pronoun() {
+    return 'it';
+  }
+
+  container() {
+    return 'bottle';
+  }
+}
+
+BottleNumber.register(BottleNumber1);
+
+class BottleNumber6 extends BottleNumber {
+  quantity() {
+    return '1';
+  }
+
+  container() {
+    return 'six-pack';
+  }
+}
+
+BottleNumber.register(BottleNumber6);
