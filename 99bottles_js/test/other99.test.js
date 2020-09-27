cdd22101c0efe1028c7c13b3512bedf9f578f717
James Miller <james.rex.miller.4@gmail.com>
2020-09-26 22:36:45 -0600

    Refactor tests, add BottleVerse test block

--- 99bottles_js/test/other99.test.js
+++ 99bottles_js/test/other99.test.js
@@ -1,15 +1,16 @@
 import { Bottles } from '../lib/other99';
 
 describe('Bottles', () => {
-  test('the first verse', () => {
-    const expected =
-      '99 bottles of beer on the wall, ' +
-      '99 bottles of beer.\n' +
-      'Take one down and pass it around, ' +
-      '98 bottles of beer on the wall.\n';
-    expect(new Bottles().verse(99)).toBe(expected);
+  describe('BottleVerse', () => {
+    test('the first verse', () => {
+      const expected =
+        '99 bottles of beer on the wall, ' +
+        '99 bottles of beer.\n' +
+        'Take one down and pass it around, ' +
+        '98 bottles of beer on the wall.\n';
+      expect(new Bottles().verse(99)).toBe(expected);
+    });
   });
-
   test('another verse', () => {
     const expected =
       '3 bottles of beer on the wall, ' +
